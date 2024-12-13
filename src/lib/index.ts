90f8a0be648fc976e53591ead6e88347efe78836
Leonard Lesinski <84378319+Le0X8@users.noreply.github.com>
2024-12-13 20:41:02 +0100

    buckets v2

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,44 +2,44 @@ import { type PluginOptions, type Plugin, defaultPluginOptions, access } from '@
 
 export const id = 'dev.neokit.buckets';
 export const defaultNamespace = 'buckets';
-export const apiVersion = 2;
-export const version = 1;
+export const apiVersion = 3;
+export const version = 2;
 
 export class BucketsPlugin {
 	metadataFn;
-  downloadFn;
-  uploadFn;
-  removeFn;
+	downloadFn;
+	uploadFn;
+	removeFn;
 
 	constructor(options: BucketsPluginOptions) {
 		this.metadataFn = options.metadataFn;
-    this.downloadFn = options.downloadFn;
-    this.uploadFn = options.uploadFn;
-    this.removeFn = options.removeFn;
+		this.downloadFn = options.downloadFn;
+		this.uploadFn = options.uploadFn;
+		this.removeFn = options.removeFn;
 	}
 
-  metadata(path: string) {
-    return this.metadataFn(path);
-  }
+	metadata(path: string) {
+		return this.metadataFn(path);
+	}
 
-  download(path: string) {
-    return this.downloadFn(path);
-  }
+	download(path: string) {
+		return this.downloadFn(path);
+	}
 
-  upload(path: string, data: ArrayBuffer) {
-    return this.uploadFn(path, data);
-  }
+	upload(path: string, data: ArrayBuffer) {
+		return this.uploadFn(path, data);
+	}
 
-  remove(path: string) {
-    return this.removeFn(path);
-  }
+	remove(path: string) {
+		return this.removeFn(path);
+	}
 }
 
 export interface BucketsPluginOptions extends PluginOptions {
 	metadataFn: (path: string) => Promise<unknown>;
-  downloadFn: (path: string) => Promise<ArrayBuffer>;
-  uploadFn: (path: string, data: ArrayBuffer) => Promise<unknown>;
-  removeFn: (path: string) => Promise<unknown>;
+	downloadFn: (path: string) => Promise<ArrayBuffer>;
+	uploadFn: (path: string, data: ArrayBuffer) => Promise<unknown>;
+	removeFn: (path: string) => Promise<unknown>;
 }
 
 export function plugin(options: BucketsPluginOptions): Plugin {
@@ -48,7 +48,7 @@ export function plugin(options: BucketsPluginOptions): Plugin {
 		version,
 		apiVersion,
 		plugin: new BucketsPlugin(options),
-		...defaultPluginOptions(options, { namespace: defaultNamespace })
+		...defaultPluginOptions(options, { namespace: defaultNamespace, allowInjection: true })
 	};
 }
 
@@ -57,17 +57,17 @@ export function metadata(path: string) {
 }
 
 export function download(path: string) {
-  return namespace(defaultNamespace).download(path);
+	return namespace(defaultNamespace).download(path);
 }
 
 export function upload(path: string, data: ArrayBuffer) {
-  return namespace(defaultNamespace).upload(path, data);
+	return namespace(defaultNamespace).upload(path, data);
 }
 
 export function remove(path: string) {
-  return namespace(defaultNamespace).remove(path);
+	return namespace(defaultNamespace).remove(path);
 }
 
 export function namespace(namespace: string) {
-  return access(id)[namespace].plugin as BucketsPlugin;
+	return access(id)[namespace].plugin as BucketsPlugin;
 }
